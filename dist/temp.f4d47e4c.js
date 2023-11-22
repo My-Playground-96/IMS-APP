a33f051c03443f969a57bfa7131ffd172db39b00
Sahan Lakmal <sahanloser@gmail.com>
2023-11-23 03:30:55 +0530

    Implement the teacher UI and connect with server

---
+++ dist/temp.f4d47e4c.js
@@ -0,0 +1,8 @@
+const txtName = document.querySelector("#txt-name");
+const txtContact = document.querySelector("#txt-contact");
+const btnAdd = document.querySelector("#btn-add");
+btnAdd.querySelector("click", ()=>{
+    alert("Clicked");
+});
+
+//# sourceMappingURL=temp.f4d47e4c.js.map
